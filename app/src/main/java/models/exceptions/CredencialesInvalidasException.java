10d8828207c518c61c09ff3d62111b7a57119a2a
Christina-Crvjl <christinacarvajal20@gmail.com>
2026-08-15 16:50:39 -0500

    Mensaje en CredencialesInvalidasException

--- app/src/main/java/models/exceptions/CredencialesInvalidasException.java
+++ app/src/main/java/models/exceptions/CredencialesInvalidasException.java
@@ -1,7 +1,13 @@
 package models.exceptions;
 
 public class CredencialesInvalidasException extends Exception{
+
+    public CredencialesInvalidasException(){
+        super("El usuario o la contraseña estan mal");
+    }
     public CredencialesInvalidasException(String mensaje){
+
         super(mensaje);
     }
+
 }
