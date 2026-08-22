47c4421e25f786f54c7e549c3b38236eb957316d
Daniela-Cedeno <danlucio@espol.edu.ec>
2026-08-22 10:06:56 -0500

    Renombrar Excepcion

--- app/src/main/java/models/exceptions/PronosticoDeTiempoException.java
+++
@@ -1,8 +0,0 @@
-package models.exceptions;
-
-public class PronosticoDeTiempoException extends Exception{
-    public PronosticoDeTiempoException(String mensaje){
-        super(mensaje);
-    }
-}
-
