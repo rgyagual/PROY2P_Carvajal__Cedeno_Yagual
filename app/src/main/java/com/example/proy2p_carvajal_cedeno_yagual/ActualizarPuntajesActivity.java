bb9f1109a46802b0dfac44e58759ed26896827fe
Ronny <g300607y@gmail.com>
2026-08-28 15:11:57 -0500

    Pantalla Actualizar Puntaje

---
+++ app/src/main/java/com/example/proy2p_carvajal_cedeno_yagual/ActualizarPuntajesActivity.java
@@ -0,0 +1,14 @@
+package com.example.proy2p_carvajal_cedeno_yagual;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActualizarPuntajesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar_puntajes);
+    }
+}
