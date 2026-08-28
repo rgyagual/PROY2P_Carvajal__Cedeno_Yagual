b6e8a0aeea61d00336e524232b2382fcdbad9887
Ronny <g300607y@gmail.com>
2026-08-28 15:35:09 -0500

    Crear Pantalla Mis Pronosticos

--- app/src/main/java/com/example/proy2p_carvajal_cedeno_yagual/MisPronosticosActivity.java
+++ app/src/main/java/com/example/proy2p_carvajal_cedeno_yagual/MisPronosticosActivity.java
@@ -1,6 +1,8 @@
 package com.example.proy2p_carvajal_cedeno_yagual;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MisPronosticosActivity extends AppCompatActivity {
 
+    LinearLayout contenedorPronosticos;
+    LinearLayout btnVolver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_mis_pronosticos);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        contenedorPronosticos = findViewById(R.id.contenedor_pronosticos);
+        btnVolver = findViewById(R.id.btnVolver);
+
     }
+
+
 }
